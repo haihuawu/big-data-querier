009bab2873d9e437cdf67e5d9f092f48741b4200
cheng <c.zhou@17hi.co>
2018-04-12 19:33:49 -0400

    add instructions

--- src/main/scala/com/bigdata/service/ColumnSimilarity.scala
+++ src/main/scala/com/bigdata/service/ColumnSimilarity.scala
@@ -5,7 +5,13 @@ package com.bigdata.service
   *
   * compare two columns with Jaccard Similarity
   *
-  * sim(c1, c2) = |c1 n c2| / \\c1 u c2|
+  * sim(c1, c2) = |c1 n c2| / |c1 u c2|
+  *
+  * how to start:
+  *
+  * 1. set up http get incoming request in com.bigdata.web.WebService
+  * 2. finish the below class, create an getSimilarity() method that returns a value between 0-1 that represents the similarity
+  * look at ColumnValue, SingleProfile for some coding example
   * */
 object ColumnSimilarity {
 
