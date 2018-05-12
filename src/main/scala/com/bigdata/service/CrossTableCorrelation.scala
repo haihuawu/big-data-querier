b1a22741a05ef4e7ff0fb1896d6ef4060bb0b9f2
Haihua Wu (Haley) <whh.annsempre@gmail.com>
2018-05-12 15:48:21 -0400

    submit correlation

--- src/main/scala/com/bigdata/service/CrossTableCorrelation.scala
+++ src/main/scala/com/bigdata/service/CrossTableCorrelation.scala
@@ -18,22 +18,24 @@ object CrossTableCorrelation {
       return Cache.getFromCache(key)
     }
     var result = ""
-    var df = ""
+
     if (table1.equals(table2)) {
-      df = getDataFrameByTable(table1).select(column1, column2).filter(col(column1) =!= 0).filter(col(column2) =!= 0)
+      var df = getDataFrameByTable(table1).select(column1, column2).filter(col(column1) =!= 0).filter(col(column2) =!= 0)
+      val rdd = df.rdd
+      val array = rdd.takeSample(false, limit)
+      val seriesX = df.select(column1).rdd
+      val seriesY = df.select(column2).rdd
+      val Double = Statistics.corr(seriesX, seriesY, "pearson")
     } else {
       val df1 = getDataFrameByTable(table1).select(column1, join).filter(col(column1) =!= 0)
       val df2 = getDataFrameByTable(table2).select(column2, join).filter(col(column2) =!= 0)
-      df = df1.join(df2, df1(join) === df2(join), "inner")
+      var df = df1.join(df2, df1(join) === df2(join), "inner")
+      val rdd = df.rdd
+      val array = rdd.takeSample(false, limit)
+      val seriesX = df.select(column1).rdd
+      val seriesY = df.select(column2).rdd
+      val Double = Statistics.corr(seriesX, seriesY, "pearson")
     }
-
-    val rdd = df.rdd
-    val array = rdd.takeSample(false, limit)
-
-    val seriesX = df.select(column1).rdd
-    val seriesY = df.select(column2).rdd
-
-    val Double = Statistics.corr(seriesX, seriesY, "pearson")
 
     result = JsonFormat.formatNumberArray(array)
 
