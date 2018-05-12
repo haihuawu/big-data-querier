6394c2b7fa176b7bbaad2282492fa286502515a8
Haihua Wu (Haley) <whh.annsempre@gmail.com>
2018-05-12 16:37:35 -0400

    submit correlation

--- src/main/scala/com/bigdata/service/CrossTableCorrelation.scala
+++ src/main/scala/com/bigdata/service/CrossTableCorrelation.scala
@@ -5,6 +5,7 @@ import com.bigdata.util.{AppConfig, Cache, JsonFormat, Util}
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions.col
 import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.mllib.linalg.Vectors
 
 object CrossTableCorrelation {
 
@@ -18,29 +19,56 @@ object CrossTableCorrelation {
       return Cache.getFromCache(key)
     }
     var result = ""
+    var correlaval = 0.0
 
     if (table1.equals(table2)) {
       var df = getDataFrameByTable(table1).select(column1, column2).filter(col(column1) =!= 0).filter(col(column2) =!= 0)
       val rdd = df.rdd
       val array = rdd.takeSample(false, limit)
-      val seriesX = df.select(column1).rdd
-      val seriesY = df.select(column2).rdd
-      val Double = Statistics.corr(seriesX, seriesY, "pearson")
+      correlaval = computeCorrelation(df, column1, column2)
     } else {
       val df1 = getDataFrameByTable(table1).select(column1, join).filter(col(column1) =!= 0)
       val df2 = getDataFrameByTable(table2).select(column2, join).filter(col(column2) =!= 0)
       var df = df1.join(df2, df1(join) === df2(join), "inner")
       val rdd = df.rdd
       val array = rdd.takeSample(false, limit)
-      val seriesX = df.select(column1).rdd
-      val seriesY = df.select(column2).rdd
-      val Double = Statistics.corr(seriesX, seriesY, "pearson")
+      correlaval = computeCorrelation(df, column1, column2)
     }
 
     result = JsonFormat.formatNumberArray(array)
+    result = addCorrelavalToResult(result, correlaval)
 
     Cache.putInCache(key, result)
     result
+  }
+
+  def addCorrelavalToResult(pre : String, correlaval : Double): String = {
+    val json = StringBuilder.newBuilder
+
+    json.append(pre.substring(0,pre.length()-1))
+    json.append(",")
+    json.append("\"correlation\" : ")
+    json.append(correlaval.toString())
+    json.append("}")
+
+    return json.toString();
+  }
+
+  def computeCorrelation(df : DataFrame, column1: String, column2: String): Double = {
+    val rddx = df.select(column1).rdd
+    val rddy = df.select(column2).rdd
+
+    val seriesX = rddx.map{
+      x =>
+        x(0).toString().toDouble
+    }
+    val seriesY = rddy.map{
+      x =>
+        x(0).toString().toDouble
+    }
+
+    val correlaval = Statistics.corr(seriesX, seriesY, "pearson")
+    correlaval
   }
 
   def getKey(table1: String, table2: String, column1: String, column2: String): String = {
